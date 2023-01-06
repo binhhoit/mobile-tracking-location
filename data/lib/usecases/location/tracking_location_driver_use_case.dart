1d1c4ff89b65e3d269d676a14cb8b0237b372a17
binh.ho <binh.hothanh@lowellonline.onmicrosoft.com>
2023-01-06 10:15:49 +0700

    -- load location in map

--- data/lib/usecases/location/tracking_location_driver_use_case.dart
+++ data/lib/usecases/location/tracking_location_driver_use_case.dart
@@ -5,10 +5,11 @@ import 'package:injectable/injectable.dart';
 import '../base/use_case.dart';
 
 @injectable
-class FirestoreUserCase extends ReactiveUseCase<DocumentSnapshot<Map<String, dynamic>>, String> {
+class TrackingLocationDriverUseCase
+    extends ReactiveUseCase<DocumentSnapshot<Map<String, dynamic>>, String> {
   final FirestoreRepository _firestoreRepository;
 
-  FirestoreUserCase(this._firestoreRepository);
+  TrackingLocationDriverUseCase(this._firestoreRepository);
 
   @override
   Future<Stream<DocumentSnapshot<Map<String, dynamic>>?>> buildUseCaseStream(String? params) async {
